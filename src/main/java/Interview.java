02b6a2dc909793264f2f6782c365c370e065e70a
Tomas <tomas.bartnykas@gmail.com>
2020-07-03 15:06:55 +0200

    Code clean up

--- src/main/java/Interview.java
+++ src/main/java/Interview.java
@@ -1,51 +1,54 @@
-import java.awt.event.TextEvent;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Interview {
 
+    public static final int ASCII_CORRECTION = 64;
+    public static final int ASCII_Z = 90 - ASCII_CORRECTION;
+    public static final int ASCII_O = 79 - ASCII_CORRECTION;
+    public static final int ASCII_R = 82 - ASCII_CORRECTION;
+    public static final int ASCII_E = 69 - ASCII_CORRECTION;
+    public static final int ASCII_N = 78 - ASCII_CORRECTION;
+    public static final int ASCII_T = 84 - ASCII_CORRECTION;
+    public static final int ASCII_W = 87 - ASCII_CORRECTION;
+    public static final int ASCII_H = 72 - ASCII_CORRECTION;
 
     public static void main(String[] args) {
        String example = "OZONETOWERTHREETWO";
        int[] array = new int[32];
-        int ASCII_correction = 64;
         for (int i = 0; i< example.length(); i++) {
-           array[example.charAt(i)- ASCII_correction]++;
+           array[example.charAt(i) - ASCII_CORRECTION]++;
        }
-        int zero = array['Z' - ASCII_correction];
+        int zero = array[ASCII_Z];
         for (int j = 0; j< zero; j++) {
            System.out.print("0");
        }
-        array[90- ASCII_correction]-=zero;
-        array[69- ASCII_correction]-=zero;
-        array[82- ASCII_correction]-=zero;
-        array[79- ASCII_correction]-=zero;
+        array[ASCII_Z]-=zero;
+        array[ASCII_E]-=zero;
+        array[ASCII_R]-=zero;
+        array[ASCII_O]-=zero;
 
-       int conditionOne = Math.min(Math.min(array[79- ASCII_correction], array[78- ASCII_correction]),array[69- ASCII_correction]);
+       int conditionOne = Math.min(Math.min(array[ASCII_O], array[ASCII_N]),array[ASCII_E]);
         for (int j=0; j<conditionOne; j++) {
             System.out.print("1");
         }
-        array[79- ASCII_correction]-=conditionOne;
-        array[78- ASCII_correction]-=conditionOne;
-        array[69- ASCII_correction]-=conditionOne;
+        array[ASCII_O]-=conditionOne;
+        array[ASCII_N]-=conditionOne;
+        array[ASCII_E]-=conditionOne;
 
-        int conditionTwo = Math.min(Math.min(array[84- ASCII_correction], array[87- ASCII_correction]),array[79- ASCII_correction]);
+        int conditionTwo = Math.min(Math.min(array[ASCII_T], array[ASCII_W]),array[ASCII_O]);
             for (int j=0; j<conditionTwo; j++) {
             System.out.print("2");
         }
-        array[84- ASCII_correction]-=conditionTwo;
-        array[87- ASCII_correction]-=conditionTwo;
-        array[79- ASCII_correction]-=conditionTwo;
+        array[ASCII_T]-=conditionTwo;
+        array[ASCII_W]-=conditionTwo;
+        array[ASCII_O]-=conditionTwo;
 
-        int conditionThree = Math.min(Math.min(Math.min(array[84- ASCII_correction], array[72- ASCII_correction]),array[82- ASCII_correction]),array[69- ASCII_correction]/2);
+        int conditionThree = Math.min(Math.min(Math.min(array[ASCII_T], array[ASCII_H]),array[ASCII_R]),array[ASCII_E]/2);
         for (int j=0; j<conditionThree; j++) {
             System.out.print("3");
         }
-        array[84- ASCII_correction]-=conditionThree;
-        array[72- ASCII_correction]-=conditionThree;
-        array[82- ASCII_correction]-=conditionThree;
-        array[69- ASCII_correction]-=conditionThree*2;
-
+        array[ASCII_T]-=conditionThree;
+        array[ASCII_H]-=conditionThree;
+        array[ASCII_R]-=conditionThree;
+        array[ASCII_E]-=conditionThree*2;
     }
 
 
