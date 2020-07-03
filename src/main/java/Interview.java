2dd983f271b9b60cbe7c7b5440ebdf51286bfda1
Tomas <tomas.bartnykas@gmail.com>
2020-07-03 15:13:23 +0200

    Code clean up

--- src/main/java/Interview.java
+++ src/main/java/Interview.java
@@ -49,6 +49,43 @@ public class Interview {
         array[ASCII_H]-=conditionThree;
         array[ASCII_R]-=conditionThree;
         array[ASCII_E]-=conditionThree*2;
+
+        int conditionFour = Math.min(Math.min(Math.min(array[ASCII_F], array[ASCII_O]),array[ASCII_U]),array[ASCII_R]);
+        for (int j=0; j<conditionFour; j++) {
+            System.out.print("4");
+        }
+        array[ASCII_F]-=conditionFour;
+        array[ASCII_O]-=conditionFour;
+        array[ASCII_U]-=conditionFour;
+        array[ASCII_R]-=conditionFour;
+
+        int conditionFive = Math.min(Math.min(Math.min(array[ASCII_F], array[ASCII_I]),array[ASCII_V]),array[ASCII_E]);
+        for (int j=0; j<conditionFive; j++) {
+            System.out.print("5");
+        }
+        array[ASCII_F]-=conditionFive;
+        array[ASCII_I]-=conditionFive;
+        array[ASCII_V]-=conditionFive;
+        array[ASCII_E]-=conditionFive;
+
+        int conditionSix = Math.min(Math.min(Math.min(array[ASCII_S], array[ASCII_I]),array[ASCII_X]));
+        for (int j=0; j<conditionSix; j++) {
+            System.out.print("6");
+        }
+        array[ASCII_S]-=conditionSix;
+        array[ASCII_I]-=conditionSix;
+        array[ASCII_X]-=conditionSix;
+
+        int conditionSeven = Math.min(Math.min(Math.min(array[ASCII_S], array[ASCII_N]),array[ASCII_V]),array[ASCII_E]/2);
+        for (int j=0; j<conditionSeven; j++) {
+            System.out.print("7");
+        }
+        array[ASCII_S]-=conditionSeven;
+        array[ASCII_E]-=conditionSeven*2;
+        array[ASCII_V]-=conditionSeven;
+        array[ASCII_N]-=conditionSeven;
+
+
     }
 
 
